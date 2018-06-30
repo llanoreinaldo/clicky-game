6217ceeb2be236610b45a595e202e2864c615297
Reinaldo Llano, Jr <llanoreinaldo@gmail.com>
2018-06-30 11:12:35 -0400

    commit initial files

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import MainPage from "./components/MainPage";
+
+//Import image files
+import antman from "./images/antman_headshot.jpg";
+import batgirl from "./images/batgirl_headshot.jpg";
+import batman from "./images/batman_headhost.jpg";
+import blackpanther from "./images/blackpanther_headshot.jpg";
+import captainamerica from "./images/captainamerica_headshot.jpg";
+import daredevil from "./images/daredevil_headshot.jpg";
+import deadpool from "./images/deadpool_headshot.jpg";
+import drstrange from "./images/drstrange_headshot.jpg";
+import flash from "./images/flash_headshot.jpg";
+import greenlantern from "./images/greenlantern_headshot.jpg";
+import hawkgirl from "./images/hawkgirl_headshot.jpg";
+import hulk from "./images/hulk_headshot.jpg";
+import ironman from "./images/ironman_headshot.jpg";
+import martianman from "./images/martian_manhunter_headshot.jpg";
+import msmarve from "./images/msmarvel_heashot.jpg";
+import spiderman from "./images/spiderman_headshot.jpg";
+import superman from "./images/superman_headshot.jpg";
+import thor from "./images/thor_headshot.jpg";
+import vision from "./images/vision_headshot.jpg";
+import wonderwoman from "./images/wonderwoman_headshot.jpg";
+
+const App = () => <MainPage />;
+
+export default App;
---
+++ my-app/src/components/MainPage.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+const MainPage = () => (
+  <div className="container">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+      Heroes Clicky-Game
+      </a>
+      <button
+        className="navbar-toggler"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+            Click an image to begin! <span className="sr-only">(current)</span>
+            </a>
+          </li>
+        </ul>
+        </div>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+            Score: <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+            Top Score: <span className="sr-only">(current)</span>
+            </a>
+          </li>
+        </ul>
+        </div>
+    </nav>
+   
+
+    <div className="jumbotron">
+      <h1>Heroes Clicky Game!</h1>
+      <p>
+      Click on an image to earn points, but don't click on any more than once!
+      </p>
+    </div>
+    <div className="card">
+      <div className="card-header">
+        <h3 className="card-title">Non eu sit duis adipisicing esse incididunt ad proident.</h3>
+      </div>
+     <footer>
+       
+       </footer>
+    </div>
+  </div>
+);
+
+export default MainPage;
